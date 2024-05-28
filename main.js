5cacbd9a314572e6249f7d498c7f782821236c63
z1a <vizarovim@gmail.com>
2024-05-28 14:12:22 -0700

    v3

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Works with uuid@3
-// import uuid from "uuid/v4"
+import uuid from "uuid/v4"
 
 // Works with uuid@8
-import { v4 } from "uuid";
+// import { v4 } from "uuid";
 
-document.querySelector("#app").innerHTML = `<p>${v4()}</p>`;
+document.querySelector("#app").innerHTML = `<p>${uuid()}</p>`;
